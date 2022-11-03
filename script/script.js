07cab3d329cef7ae2b10426f9c3fd44e4bfec25a
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-03 18:46:28 +0100

    styled cards and try to add background color on cards

--- script/script.js
+++ script/script.js
@@ -20,11 +20,14 @@ fetch(apiUrl) // Hämtar API
         })
         .then((pokemon) => {
 
+          const pokemonType = pokemon.types[0].type.name = "grass";
+
+
           const cardContainer = document.createElement("div");
           const pokemonCard = document.createElement("div");
           const cardTitle = document.createElement("span");
           const image = document.createElement("div");
-          const ulList = document.createElement("ul");
+          const info = document.createElement("article");
           const cardFooter = document.createElement("div");
           const addBtn = document.createElement("button");
 
@@ -32,22 +35,29 @@ fetch(apiUrl) // Hämtar API
           pokemonCard.classList.add("card", "pokemon-card");
           cardTitle.classList.add("card-title");
           image.classList.add("card-img-top");
-          ulList.classList.add("list-group", "list-group-flush");
-          cardFooter.classList.add("card-footer");
+          info.classList.add("card-info");
+          cardFooter.classList.add("cardfooter");
           addBtn.classList.add("addbtn");
 
           image.innerHTML = `<img src=${pokemon.sprites.front_default}>`;
-          cardTitle.innerHTML = `<h6>${pokemon.name}</h6><h6 xclass="hp">${pokemon.stats[0].base_stat} HP</h6>`;
+          cardTitle.innerHTML = `<h6>${pokemon.name}</h6><h6 class="hp">
+            ${pokemon.stats[0].base_stat} HP</h6>
+          `;
+          info.innerHTML = `<p>ddwdwdwdwdw</p>`;
+          cardFooter.innerHTML = `<p>${pokemon.types[0].type.name}</p>`;
           addBtn.innerHTML = `Add`;
 
           cardContainer.appendChild(pokemonCard);
           cardContainer.appendChild(addBtn);
           pokemonCard.appendChild(cardTitle);
           pokemonCard.appendChild(image);
-          pokemonCard.appendChild(ulList);
+          pokemonCard.appendChild(info);
+          pokemonCard.appendChild(cardFooter);
           products.appendChild(cardContainer);
 
-          console.log(pokemon);
+          if (pokemonType == "grass") {
+            pokemonCard.setAttributes("class", "grass")
+          }
         });
     }
   });
