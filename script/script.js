82faa7db8427434ba527764e8d4d5bb57b1e9b61
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-03 10:18:52 +0100

    added pokemon API

--- script/script.js
+++ script/script.js
@@ -1,16 +1,38 @@
-const bodyElement = document.querySelector("body")
-const navbarElement = document.querySelector(".navbar")
-const mainElement = document.querySelector("main");
-const footerElement = document.querySelector("footer");
-const url = "https://pokeapi.co/api/v2/";
-
-fetch(url)  // Hämtar Data från API
-  .then((response) => { // Väntar på att få svar på hämting är OK
-    return response.json(); // Skickar tillabak info som en json
+const main = document.querySelector("main");
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=15&offset=0';
+fetch(url)
+  .then((response) => {
+    return response.json();
   })
-  .then((data) => { // sparar json som data
-    let beerData = data // tilldelar data från API till beerData 
-    beerData.map(function (beerData) {
-      console.log(beerData)
-    })
-  });
+  .then((data) => {
+    const pokemonData = data.results;
+    pokemonData.forEach(item => {
+      let pokemonUrl = item.url;
+      let pokeName = item.name;
+
+      const card = document.createElement("article");
+
+      card.innerHTML = pokeName;
+
+      main.append(card);
+
+      fetch(pokemonUrl)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          let pokedata = data.id;
+
+          const id = document.createElement("p")
+
+          id.innerHTML = pokedata;
+
+          card.prepend(id);
+        })
+    });
+  })
+
+
+
+
+
