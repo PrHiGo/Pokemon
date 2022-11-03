5a47b92d905760a7b81544ca3ac14e4f089b4dd0
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-03 11:29:03 +0100

    creating pokemon cards

--- script/script.js
+++ script/script.js
@@ -1,33 +1,43 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector("navbar");
 const main = document.querySelector("main");
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=15&offset=0';
-fetch(url)
-  .then((response) => {
-    return response.json();
+const cardContainer = document.querySelector(".card-container");
+const footer = document.querySelector("footer");
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
+fetch(apiUrl) // Hämtar API
+  .then((response) => { // Väntar på svar
+    return response.json(); // Retunerar svar som json
   })
-  .then((data) => {
+  .then((data) => { // Sparar json i data
     const pokemonData = data.results;
-    pokemonData.forEach(item => {
-      let pokemonUrl = item.url;
-      let pokeName = item.name;
+    pokemonData.map(item => {
+      const pokemonUrl = item.url; // URL till varje enskild Pokemon
+      let pokemonName = item.name;
 
-      const card = document.createElement("article");
+      const pokemonCard = document.createElement("div");
 
-      card.innerHTML = pokeName;
+      pokemonCard.classList.add("card", "product-card");
 
-      main.append(card);
+      pokemonCard.innerHTML = pokemonName;
 
-      fetch(pokemonUrl)
+      cardContainer.append(pokemonCard);
+
+      fetch(pokemonUrl) // Hämtar data om varje pokemon
         .then((response) => {
-          return response.json()
+          return response.json();
         })
-        .then((data) => {
-          let pokedata = data.id;
+        .then((singleData) => {
+          const pokemonSingleData = singleData; // Detaljerad data om varje enskild pokemon
 
-          const id = document.createElement("p")
+          const image = document.createElement("div");
+          const ulList = document.createElement("ul");
+          const cardFooter = document.createElement("div");
+          const buyBtn = document.createElement("button");
 
-          id.innerHTML = pokedata;
-
-          card.prepend(id);
+          ulList.classList.add("list-group", "list-group-flush");
+          cardFooter.classList.add("card-footer");
+          image.classList.add("card-img-top");
+          buyBtn.classList.add("buybtn");
         })
     });
   })
