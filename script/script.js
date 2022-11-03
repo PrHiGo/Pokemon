29d8a71a62ddd9d2fe2757d73147671dd2944f38
Thomas Palmqvist <thomas_palmqvist@live.se>
2022-11-03 12:44:59 +0100

    added pictures and started to style cards

--- script/script.js
+++ script/script.js
@@ -4,43 +4,42 @@ const main = document.querySelector("main");
 const cardContainer = document.querySelector(".card-container");
 const footer = document.querySelector("footer");
 const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
+const myPokemon = [];
+
 fetch(apiUrl) // Hämtar API
   .then((response) => { // Väntar på svar
     return response.json(); // Retunerar svar som json
   })
-  .then((data) => { // Sparar json i data
-    const pokemonData = data.results;
-    pokemonData.map(item => {
-      const pokemonUrl = item.url; // URL till varje enskild Pokemon
-      let pokemonName = item.name;
-
-      const pokemonCard = document.createElement("div");
-
-      pokemonCard.classList.add("card", "product-card");
-
-      pokemonCard.innerHTML = pokemonName;
-
-      cardContainer.append(pokemonCard);
+  .then((pokemonData) => { // Sparar json i data
+    for (let info of pokemonData.results) {
+      const pokemonUrl = info.url; // URL till varje enskild Pokemon
 
       fetch(pokemonUrl) // Hämtar data om varje pokemon
         .then((response) => {
           return response.json();
         })
-        .then((singleData) => {
-          const pokemonSingleData = singleData; // Detaljerad data om varje enskild pokemon
+        .then((pokemon) => {
 
+          const pokemonCard = document.createElement("div");
           const image = document.createElement("div");
           const ulList = document.createElement("ul");
           const cardFooter = document.createElement("div");
           const buyBtn = document.createElement("button");
 
+          pokemonCard.classList.add("card", "product-card");
+          image.classList.add("card-img-top");
           ulList.classList.add("list-group", "list-group-flush");
           cardFooter.classList.add("card-footer");
-          image.classList.add("card-img-top");
           buyBtn.classList.add("buybtn");
-        })
-    });
-  })
+
+          image.innerHTML = `<h5>${pokemon.name}</h5><img src=${pokemon.sprites.front_default}>`;
+
+          pokemonCard.appendChild(image);
+          cardContainer.append(pokemonCard);
+
+        });
+    }
+  });
 
 
 
